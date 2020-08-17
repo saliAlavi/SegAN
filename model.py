d475ba99319b6580bb2e5bcd94b88d72acca194a
salialavi <sa.alavi.b@gmail.com>
2020-08-17 08:41:41 +0430

    minor changes

--- model.py
+++ model.py
@@ -6,6 +6,27 @@ from math import sqrt
 LEAKAGE = 0.001
 
 
+class GConvBlock(nn.Module):
+    def __init__(self, in_dim, out_dim, kernel_size):
+        super(GConvBlock, self).__init__()
+        pad0 = (kernel_size[0] - 1) / 2
+        pad1 = (kernel_size[1] - 1) / 2
+
+        self.conv_l1 = nn.Conv2d(in_dim, out_dim, kernel_size=(kernel_size[0], 1), padding=(pad0, 0))
+        self.conv_l2 = nn.Conv2d(out_dim, out_dim, kernel_size=(1, kernel_size[1]), padding=(0, pad1))
+        self.conv_r1 = nn.Conv2d(in_dim, out_dim, kernel_size=(1, kernel_size[1]), padding=(0, pad1))
+        self.conv_r2 = nn.Conv2d(out_dim, out_dim, kernel_size=(kernel_size[0], 1), padding=(pad0, 0))
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.normal_(1.0, 0.02)
+                m.bias.data.fill_(0)
+
+
 class ConvBlock(nn.Module):
     def __init__(self, in_dim, out_dim, kernel, stride, padding=1):
         super(ConvBlock, self).__init__()
@@ -19,14 +40,12 @@ class ConvBlock(nn.Module):
         return residual
 
 
-
 class DeconvBlock(nn.Module):
     def __init__(self, in_dim, out_dim, kernel, stride, padding=1, scale_factor=2):
         super(DeconvBlock, self).__init__()
-        self.upsample = nn.Upsample(scale_factor=scale_factor, mode = 'bilinear')
+        self.upsample = nn.Upsample(scale_factor=scale_factor, mode='bilinear')
         self.conv = nn.Conv2d(in_dim, out_dim, (kernel, kernel), stride=stride, padding=padding)
         self.batchnorm = nn.BatchNorm2d(out_dim)
-
 
     def forward(self, x):
         residual = self.upsample(x)
@@ -55,7 +74,6 @@ class Segmentor(nn.Module):
             nn.Conv2d(64, d_out, 3, 1, padding=1)
         )
 
-
     def forward(self, x):
         res_1 = self.conv_1(x)
         res_1 = F.leaky_relu(res_1)
@@ -74,39 +92,12 @@ class Segmentor(nn.Module):
         res_8 = self.deconv_4(res_7)
         return res_8
 
-
-
-
-class GlobalConvBlock(nn.Module):
-    def __init__(self, in_dim, out_dim, kernel_size):
-        super(GlobalConvBlock, self).__init__()
-        pad0 = (kernel_size[0] - 1) / 2
-        pad1 = (kernel_size[1] - 1) / 2
-
-        self.conv_l1 = nn.Conv2d(in_dim, out_dim, kernel_size=(kernel_size[0], 1),
-                                 padding=(pad0, 0))
-        self.conv_l2 = nn.Conv2d(out_dim, out_dim, kernel_size=(1, kernel_size[1]),
-                                 padding=(0, pad1))
-        self.conv_r1 = nn.Conv2d(in_dim, out_dim, kernel_size=(1, kernel_size[1]),
-                                 padding=(0, pad1))
-        self.conv_r2 = nn.Conv2d(out_dim, out_dim, kernel_size=(kernel_size[0], 1),
-                                 padding=(pad0, 0))
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, sqrt(2. / n))
-                if m.bias is not None:
-                    m.bias.data.zero_()
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.normal_(1.0, 0.02)
-                m.bias.data.fill_(0)
-
     def forward(self, x):
         x_l = self.conv_l1(x)
         x_l = self.conv_l2(x_l)
         x_r = self.conv_r1(x)
         x_r = self.conv_r2(x_r)
-        #combine two paths
+        # combine two paths
         x = x_l + x_r
         return x
 
@@ -114,94 +105,63 @@ class GlobalConvBlock(nn.Module):
 class Critic(nn.Module):
     def __init__(self, d_in):
         super(Critic, self).__init__()
-        ndf=64
+        ndf = 64
         self.convblock1 = nn.Sequential(
-            # input is (channel_dim) x 128 x 128
             nn.Conv2d(d_in, ndf, 7, 2, 3, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            # nn.Dropout2d(p=0.2),
-            # state size. (ndf) x 64 x 64
         )
         self.convblock1_1 = nn.Sequential(
-            # state size. (ndf) x 64 x 64
-            GlobalConvBlock(ndf, ndf * 2, (13, 13)),
-            # nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            GConvBlock(ndf, ndf * 2, (13, 13)),
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            # nn.Dropout2d(p=0.2),
-            # state size. (ndf*2) x 64 x 64
         )
         self.convblock2 = nn.Sequential(
-            # state size. (ndf * 2) x 64 x 64
             nn.Conv2d(ndf * 1, ndf * 2, 5, 2, 2, bias=False),
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            # nn.Dropout2d(p=0.2),
-            # state size. (ndf*2) x 32 x 32
         )
         self.convblock2_1 = nn.Sequential(
-            # input is (ndf*2) x 32 x 32
-            GlobalConvBlock(ndf * 2, ndf * 4, (11, 11)),
+            GConvBlock(ndf * 2, ndf * 4, (11, 11)),
             nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
-            # nn.Dropout2d(p=0.2),
-            # state size. (ndf*4) x 32 x 32
         )
         self.convblock3 = nn.Sequential(
-            # state size. (ndf * 4) x 32 x 32
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
-            # nn.Dropout2d(p=0.2),
-            # state size. (ndf*4) x 16 x 16
         )
         self.convblock3_1 = nn.Sequential(
-            GlobalConvBlock(ndf * 4, ndf * 8, (9, 9)),
+            GConvBlock(ndf * 4, ndf * 8, (9, 9)),
             nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
 
         )
         self.convblock4 = nn.Sequential(
-            # state size. (ndf*4) x 16 x 16
             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
-            # nn.Dropout2d(p=0.2),
-            # state size. (ndf*8) x 8 x 8
         )
         self.convblock4_1 = nn.Sequential(
-            # input is (ndf*8) x 8 x 8
-            GlobalConvBlock(ndf * 8, ndf * 16, (7, 7)),
+            GConvBlock(ndf * 8, ndf * 16, (7, 7)),
             nn.BatchNorm2d(ndf * 16),
             nn.LeakyReLU(0.2, inplace=True),
-            # nn.Dropout2d(p=0.2),
-            # state size. (ndf*16) x 8 x 8
         )
         self.convblock5 = nn.Sequential(
             # state size. (ndf*8) x 8 x 8
             nn.Conv2d(ndf * 8, ndf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
-            # nn.Dropout2d(p=0.2),
-            # state size. (ndf*16) x 4 x 4
         )
         self.convblock5_1 = nn.Sequential(
-            # input is (ndf*16) x 4 x 4
             nn.Conv2d(ndf * 16, ndf * 32, 3, 1, 1, bias=False),
             nn.BatchNorm2d(ndf * 32),
             nn.LeakyReLU(0.2, inplace=True),
-            # nn.Dropout2d(p=0.2),
-            # state size. (ndf*32) x 4 x 4
         )
         self.convblock6 = nn.Sequential(
-            # state size. (ndf*32) x 4 x 4
             nn.Conv2d(ndf * 8, ndf * 8, 3, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
-            # nn.Dropout2d(p=0.2),
-            # state size. (ndf*32) x 2 x 2
         )
-        # self._initialize_weights()
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
@@ -216,20 +176,14 @@ class Critic(nn.Module):
 
         batchsize = input.size()[0]
         out1 = self.convblock1(input)
-        # out1 = self.convblock1_1(out1)
         out2 = self.convblock2(out1)
-        # out2 = self.convblock2_1(out2)
         out3 = self.convblock3(out2)
-        # out3 = self.convblock3_1(out3)
         out4 = self.convblock4(out3)
-        # out4 = self.convblock4_1(out4)
         out5 = self.convblock5(out4)
-        # out5 = self.convblock5_1(out5)
         out6 = self.convblock6(out5)
-        # out6 = self.convblock6_1(out6) + out6
-        output = torch.cat((input.view(batchsize,-1),1*out1.view(batchsize,-1),
-                            2*out2.view(batchsize,-1),2*out3.view(batchsize,-1),
-                            2*out4.view(batchsize,-1),2*out5.view(batchsize,-1),
-                            4*out6.view(batchsize,-1)),1)
+        output = torch.cat((input.view(batchsize, -1), 1 * out1.view(batchsize, -1),
+                            2 * out2.view(batchsize, -1), 2 * out3.view(batchsize, -1),
+                            2 * out4.view(batchsize, -1), 2 * out5.view(batchsize, -1),
+                            4 * out6.view(batchsize, -1)), 1)
 
         return output
